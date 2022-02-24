08ab99bc89d58db375f8ae018a743b0849bc958f
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2022-02-23 22:12:55 -0300

    Finalizando o CRUD, sem melhorias

--- src/main/java/br/com/tech4me/tech4books/controller/LivroController.java
+++ src/main/java/br/com/tech4me/tech4books/controller/LivroController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,9 +47,16 @@ public class LivroController {
         return new ResponseEntity<>(repositorio.save(livro), HttpStatus.CREATED);
     }
 
-    @DeleteMapping
-    public ResponseEntity<Livro> atualizarLivro(@RequestBody Livro livro) {
-        //Teste com nova branch
-        return new ResponseEntity<>(repositorio.save(livro), HttpStatus.CREATED);
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluirLivro(@PathVariable String id) {
+        repositorio.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Livro> atualizarLivro(@PathVariable String id, @RequestBody Livro livro) {
+        livro.setId(id);
+        return new ResponseEntity<>(repositorio.save(livro), HttpStatus.ACCEPTED);
+    }
+
 }
